8e56f80b8b252db474daaecf9037e3bb842b1ceb
WaterMagent <3846359425@qq.com>
2026-06-27 10:55:47 +0800

    添加滚动歌词卡片

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   outDir: 'build',
+  vite: {
+    server: {
+      proxy: {
+        '/api/music/lyric': {
+          target: 'https://music.163.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace('/api/music/lyric', '/api/song/lyric'),
+        },
+      },
+    },
+  },
 });
